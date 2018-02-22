0d10dae9f0aa83616877d2437d8cce57432e1b59
Karthik Praturu <kart.prat@gmail.com>
2018-02-21 22:17:51 -0500

    Updated website with projects from last year

--- js/scriptProjects.js
+++ js/scriptProjects.js
@@ -5,6 +5,13 @@ function unhoverA(element) {
     element.setAttribute('src', '../img/attackSquares.png');
 }
 
+function hoverB(element) {
+    element.setAttribute('src', '../img/bobbin.gif');
+}
+function unhoverB(element) {
+    element.setAttribute('src', '../img/bobbin.png');
+}
+
 function hoverC(element) {
     element.setAttribute('src', '../img/CAD.gif');
 }
